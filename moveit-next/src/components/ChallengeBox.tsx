02b82a7094eb16fad5e6a9230564c03a222255da
hstrada <h.strada@hotmail.com>
2021-02-25 09:11:18 -0300

    chore: create countdown provider

--- moveit-next/src/components/ChallengeBox.tsx
+++ moveit-next/src/components/ChallengeBox.tsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 
 import styles from '../styles/components/ChallengeBox.module.css';
 
@@ -7,6 +8,18 @@ export function ChallengeBox() {
   const { activeChallenge, resetChallenge, completeChallenge } = useContext(
     ChallengesContext
   );
+
+  const { resetCountdown } = useContext(CountdownContext);
+
+  function handleChallengeSucceeded() {
+    completeChallenge();
+    resetCountdown();
+  }
+
+  function handleChallengeFailed() {
+    resetChallenge();
+    resetCountdown();
+  }
 
   return (
     <div className={styles.challengeBoxContainer}>
@@ -24,13 +37,13 @@ export function ChallengeBox() {
             <button
               type="button"
               className={styles.challengeFailedButton}
-              onClick={resetChallenge}
+              onClick={handleChallengeFailed}
             >
               Falhei
             </button>
             <button
               type="button"
-              onClick={completeChallenge}
+              onClick={handleChallengeSucceeded}
               className={styles.challengeSucceededButton}
             >
               Completei
